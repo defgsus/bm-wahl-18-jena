3f9c6ca470d62922faa6ad887e55dd3a65443f49
bergi <s.berke@netzkolchose.de>
2018-05-09 14:20:38 +0200

    some more testing with geo-located society-statistics

--- src/tools/data.py
+++ src/tools/data.py
@@ -146,7 +146,9 @@ def load_pandas_bmwahl(fileid):
     return df
 
 
-def load_pandas_stat():
+def load_pandas_stat(year=None):
+    if year is not None:
+        year = str(year)
 
     data = load_json("stats-index")
 
@@ -158,15 +160,34 @@ def load_pandas_stat():
     for theme in geographies["themes"]:
         url_part = theme["fileName"].split("/")[-1]
         fileid = "stat-%(themeId)s" % theme
-
-        data = load_json("%s.json" % fileid,
-                         "http://statistiken.jena.de/instantatlas/stadtbezirksstatistik/%s" % url_part)
+        DATA_FILES[fileid] = {
+            "filename": "%s.json" % fileid,
+            "url": "http://statistiken.jena.de/instantatlas/stadtbezirksstatistik/%s" % url_part
+        }
+        data = load_json(fileid)
         for indicator in data["indicators"]:
+            if year is not None and indicator["date"] != year:
+                continue
             dic["%(name)s(%(date)s)" % indicator] = indicator["values"]
 
     df = pd.DataFrame(dic)
     df.index = df["Bezirk"]
     del df["Bezirk"]
+    return df
+
+
+def calc_bmwahl_percent(bm):
+    df = bm.copy()
+    # Stimmen für Parteien in Prozent der gültigen Stimmen
+    for col in bm.columns:
+        if not col.startswith("n"):
+            df[col] = df[col] / df["ng"].clip(1) * 100
+    # gültige und ungültige Stimmen in Prozent der Wähler
+    df["ng"] = df["ng"] / df["nw"].clip(1) * 100
+    df["nu"] = df["nu"] / df["nw"].clip(1) * 100
+    # Wähler in Prozent der Wahlberechtigten
+    if max(df["n"]):
+        df["nw"] = df["nw"] / df["n"].clip(1) * 100
     return df
 
 
@@ -187,6 +208,41 @@ def rename_bmwahl(df):
         'Arne Petrich': "ARNE",
     }
     return df.copy().rename(columns={c:mapping.get(c, c) for c in df.columns})
+
+
+def get_bmwahl_by_stats(bm):
+    """Reorganize voting-data in bm to the structure of the Statistikbezirke using overlapping polygons"""
+    from .geodata import GeoData
+    geo = GeoData("bm-shape")
+    geo2 = GeoData("stats-shape")
+
+    bm_poly_map = {str(int(k.split()[0])):bm.loc[k] for k in bm.index}
+
+    #df = pd.DataFrame(columns=bm.columns, index="Bezirk")
+    skey_sum_dic = dict()
+    for wkey in geo.polygons:
+        wpoly = geo.polygons[wkey]
+        row = bm_poly_map[wkey]
+        for skey in geo2.polygons:
+            spoly = geo2.polygons[skey]
+            if spoly["polygon"].intersects(wpoly["polygon"]):
+                try:
+                    intersection = spoly["polygon"].intersection(wpoly["polygon"])
+                    shared_area = intersection.area
+                except:
+                    shared_area = 0
+                if skey not in skey_sum_dic:
+                    skey_sum_dic[skey] = [0] * bm.shape[0]
+                sums = skey_sum_dic[skey]
+                fac = shared_area / wpoly["area"]
+                for i, val in enumerate(row):
+                    sums[i] += int(round(val * fac))
+
+    df = pd.DataFrame({bm.columns[x]: [skey_sum_dic[skey][x] for y, skey in enumerate(sorted(skey_sum_dic))]
+                       for x in range(len(bm.columns))},
+                      index=sorted(skey_sum_dic), columns=bm.columns)
+    #print(skey, wkey)
+    return df
 
 
 if __name__ == "__main__":
